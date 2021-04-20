d9aa7d47eb4608a7a4db7558409d26495250605c
chimbox <chimboxsoftgve@gmail.com>
2021-04-19 23:31:20 -0600

    Se inicializó el repositorio de la interfaz gráfica del trabajador de salud

---
+++ src/equipo_trabajadorsalud/Equipo_TrabajadorSalud.java
@@ -0,0 +1,16 @@
+package equipo_trabajadorsalud;
+
+/**
+ *
+ * @author Alfonso Felix
+ */
+public class Equipo_TrabajadorSalud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
